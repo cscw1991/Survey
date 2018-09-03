c0e5490955f42d0ceda94e5d9c9d71d8775646e2
Christian Steinke <9kiyoshi9@gmail.com>
2018-09-03 03:18:27 -0500

    Add files via upload
    Full C#

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Survey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What Is Your Name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("What Is Your Age?");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("What Month Were You Born In?");
+            var month = Console.ReadLine();
+
+            Console.WriteLine("Your Name Is: {0}", name);
+            Console.WriteLine("What Is Your Age: {0}", age);
+            Console.WriteLine("What Month Were You Born In: {0}", month);
+
+            if (month == "march")
+            {
+                Console.WriteLine("You Are a Aries.");
+            }
+            else if (month == "april")
+            {
+                Console.WriteLine("You Are a Taurus.");
+            }
+            else if (month == "may")
+            {
+                Console.WriteLine("You Are a Gemini.");
+
+            }
+        }
+    }
+}
